ad629a0612cc54e90bd04083dcf4dc2ed0db0550
David Linse <davidlinse@gmail.com>
2014-05-09 20:50:01 +0200

    refactor: move cli-option handling to #parse()

--- index.js
+++ index.js
@@ -7,40 +7,8 @@ module.exports.GitContributors = process.env.MOCHA_COV ?
 
 if (!module.parent) {
 
-  var program = require('commander');
   var fs      = require('fs');
   var version = JSON.parse(fs.readFileSync('package.json', 'utf-8')).version;
 
-  program
-    .version(version)
-    .on('--help',
-      function () {
-        console.log('  Usage:');
-        console.log('');
-        console.log('    $ git-contributors </path/to/repository>');
-        console.log('');
-      })
-    .parse(process.argv);
-
-
-  if (!program.args.length) {
-    program.help();
-    process.exit(0);
-  }
-
-  var target = program.args[0];
-
-  var GC = module.exports.GitContributors;
-
-  GC.list(target, function (err, data) {
-
-    if (err) {
-      console.log(err);
-      process.exit(1);
-    }
-
-    console.log(data);
-
-    process.exit(0);
-  });
+  module.exports.GitContributors.parse(process.argv, version);
 }
